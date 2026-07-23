7623ada2d27bcc3ba08405348e9b5e9bbbaebeb3
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2026-07-23 12:34:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -45,30 +45,30 @@ self.addEventListener('notificationclick', (e) => {
     })());
 });
 
-/* NEW: Listen for incoming Push Events (Background Wakeups) */
-self.addEventListener('push', (e) => {
-    let data = { title: 'ZenDocs Reminder', body: 'You have a scheduled note reminder.' };
+// /* NEW: Listen for incoming Push Events (Background Wakeups) */
+// self.addEventListener('push', (e) => {
+//     let data = { title: 'ZenDocs Reminder', body: 'You have a scheduled note reminder.' };
     
-    if (e.data) {
-        try { 
-            data = e.data.json(); 
-        } catch(err) { 
-            data.body = e.data.text(); 
-        }
-    }
+//     if (e.data) {
+//         try { 
+//             data = e.data.json(); 
+//         } catch(err) { 
+//             data.body = e.data.text(); 
+//         }
+//     }
 
-    const options = {
-        body: data.body,
-        icon: './icon-192.png',
-        badge: './icon-192.png',
-        tag: data.tag || 'zd-reminder',
-        renotify: true,
-        vibrate: [200, 100, 200],
-        actions: [
-            { action: 'open', title: 'Open note' },
-            { action: 'snooze', title: 'Snooze 10 min' }
-        ]
-    };
+//     const options = {
+//         body: data.body,
+//         icon: './icon-192.png',
+//         badge: './icon-192.png',
+//         tag: data.tag || 'zd-reminder',
+//         renotify: true,
+//         vibrate: [200, 100, 200],
+//         actions: [
+//             { action: 'open', title: 'Open note' },
+//             { action: 'snooze', title: 'Snooze 10 min' }
+//         ]
+//     };
 
-    e.waitUntil(self.registration.showNotification(data.title, options));
-});
+//     e.waitUntil(self.registration.showNotification(data.title, options));
+// });
